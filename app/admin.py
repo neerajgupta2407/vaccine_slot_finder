22bf6c94ceedace2cbe9f328628a23efe5516f8b
neeraj <neerajgupta2407@gmail.com>
2021-05-09 00:14:13 +0530

    Admin view

--- app/admin.py
+++ app/admin.py
@@ -1,1 +1,22 @@
 # Register your models here.
+from django.contrib import admin
+
+from app.models import TelegramAccount
+
+
+class TelegramAccountAdmin(admin.ModelAdmin):
+    list_display = [
+        "username",
+        "chat_id",
+        "pincode",
+        "registered_18",
+        "registered_45",
+        "alerts_18",
+        "alerts_45",
+    ]
+    list_filter = ["pincode", "registered_18", "registered_45"]
+    search_fields = ["pincode", "username"]
+    date_hierarchy = "create_time"
+
+
+admin.site.register(TelegramAccount, TelegramAccountAdmin)
--- vaccine_slots/settings.py
+++ vaccine_slots/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("allowed_hosts").split(",")
 
 
 # Application definition
