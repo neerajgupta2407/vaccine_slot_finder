80e86c3dcb685957637e6a61aa6c116708dde83f
neeraj <neerajgupta2407@gmail.com>
2021-05-15 13:07:12 +0530

    Added Timezone IST

--- vaccine_slots/settings.py
+++ vaccine_slots/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
@@ -127,3 +127,29 @@ else:
     telegram_bot = None
 
 MIN_SLOTS = int(config("min_slots"))
+
+LOGGING = {
+    "disable_existing_loggers": False,
+    "version": 1,
+    "handlers": {
+        "console": {
+            # logging handler that outputs log messages to terminal
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",  # message level to be written to console
+        }
+    },
+    "loggers": {
+        "": {
+            # this sets root level logger to log debug and higher level
+            # logs to console. All other loggers inherit settings from
+            # root level logger.
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,  # this tells logger to send logging message
+            # to its parent (will send if set to True)
+        },
+        "django.db": {
+            # django also has database level logging
+        },
+    },
+}
